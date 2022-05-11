3b795edb64770f8ba40a44422b42b3cbc90671fc
vaad19uj <vaad19uj@student.ju.se>
2022-05-11 14:16:14 +0200

    Update Backend_communication.py

--- raspberry pi/Backend_communication.py
+++ raspberry pi/Backend_communication.py
@@ -10,11 +10,10 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.flush()
 ser.reset_input_buffer()
 camera = PiCamera()
-camera = None
 journeyId = -2
 mowerId = 2
-coordinateId = -2
-baseURL = 'http://ec2-16-170-167-138.eu-north-1.compute.amazonaws.com/api/v1/'
+coordinateId = -4
+baseURL = 'http://ec2-16-170-167-138.eu-north-1.compute.amazonaws.com/api/v1'
 
 def takePicture(camera):
     camera.resolution = (1080, 768)
@@ -32,12 +31,15 @@ def takePicture(camera):
 def obstacleDetected(camera, x, y):
     sendCoordinate(x, y)
     url = baseURL + '/events'
-    payload = {'coordinate_id': coordinateId, 'event_type': 'Obstacle'}
+    payload = {'coordinate_id': coordinateId, 'event_type': 'Obstacle', 'mower_id': mowerId}
     files = takePicture(camera)
 
     response = requests.request("POST", url, data=payload, files=files)
+    print("obstacle detected response: ", response)
 
 def sendCoordinate(x, y):
+    global coordinateId
+    print("send coord journey id: ", journeyId)
     url = baseURL + '/coordinates'
     payload = json.dumps({
       "x": x,
@@ -48,10 +50,15 @@ def sendCoordinate(x, y):
       'Content-Type': 'application/json'
     }
 
-    coordinateId = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print("send coordinate response: ", response)
+    data = response.json()
+    coordinateId = data["coordinate_id"]
+    print("coord id: ", coordinateId)
 
 def startJourney():
-    url = baseURL + 'start-journey'
+    global journeyId
+    url = baseURL + '/journeys/start-journey'
 
     payload = json.dumps({
       "mower_id": mowerId
@@ -61,10 +68,14 @@ def startJourney():
       'Content-Type': 'application/json'
     }
 
-    journeyId = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print("startJourney response: ", response)
+    data = response.json()
+    journeyId = data["journey_id"]
+    print("journey id: ", journeyId)
 
 def stopJourney(x, y):
-    url = baseURL + 'stop-journey'
+    url = baseURL + '/journeys/stop-journey'
 
     payload = json.dumps({
       "journey_id": journeyId,
@@ -76,27 +87,35 @@ def stopJourney(x, y):
     }
 
     response = requests.request("PUT", url, headers=headers, data=payload)
-
+    print("stopJourney response")
 
 while True:
     cmd = ser.readline()
+    print(cmd)
     try:
         cmdAsInt = int(cmd)
         if cmdAsInt == 0:
             x = ser.readline()
+            print(x)
             y = ser.readline()
+            print(y)
             obstacleDetected(camera, x, y)
         elif cmdAsInt == 1:
             x = ser.readline()
+            print(x)
             y = ser.readline()
+            print(y)
             sendCoordinate(x, y)
         elif cmdAsInt == 2:
             startJourney()
         elif cmdAsInt == 3:
             x = ser.readline()
+            print(x)
             y = ser.readline()
+            print(y)
             stopJourney(x, y)
     except KeyboardInterrupt:
+	    print("Backend communication going down")
         break
     except:
         pass
